a159defa7ee655192493f274b4784b4a6ad4ccaa
xenon_d62 <xannrlcs@gmail.com>
2026-05-17 17:37:21 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        cyber: {
+          dark: '#030312',
+          purple: '#bc3bf7',
+          cyan: '#06b6d4',
+        }
+      },
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'],
+        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
